515a42817331e8dcd5af8b3c4d5e09d94c8043f4
luckwei <luckwei123@gmail.com>
2022-11-11 11:36:56 +1100

    Isolate education card component

---
+++ src/components/EducationCard.jsx
@@ -0,0 +1,32 @@
+import travelMarker from "../assets/images/travel_marker.svg";
+import {
+  card,
+  cardCountry,
+  cardDesc,
+  cardIc,
+  cardImg,
+  cardLocation,
+  cardTime,
+  cardTitle,
+} from "../styles/components/EducationCard.module.css";
+
+export default function EducationCard(props) {
+  const { imgSrc, country, name, timeFrom, timeTo, desc } = props.item;
+  return (
+    // imgSrc country name from to desc
+    <div className={card}>
+      <img className={cardImg} src={imgSrc} />
+
+      <div className={cardLocation}>
+        <img className={cardIc} src={travelMarker} />
+        <h4 className={cardCountry}>{country}</h4>
+      </div>
+
+      <h2 className={cardTitle}>{name}</h2>
+      <h3 className={cardTime}>
+        {timeFrom} - {timeTo}
+      </h3>
+      <p className={cardDesc}>{desc}</p>
+    </div>
+  );
+}
--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,28 +1,6 @@
-import travelMarker from "../assets/images/travel_marker.svg";
-import EducationCSS from "../styles/pages/Education.module.css";
-
+import EducationCard from "../components/EducationCard"
+import EducationCSS from "../styles/pages/Education.module.css"
 import EducationData from "../data/education";
-
-function EducationCard(props) {
-  const { imgSrc, country, name, timeFrom, timeTo, desc } = props.item;
-  return (
-    // imgSrc country name from to desc
-    <div className={EducationCSS.card}>
-      <img className={EducationCSS.cardImg} src={imgSrc} />
-
-      <div className={EducationCSS.cardLocation}>
-        <img className={EducationCSS.cardIc} src={travelMarker} />
-        <h4 className={EducationCSS.cardCountry}>{country}</h4>
-      </div>
-
-      <h2 className={EducationCSS.cardTitle}>{name}</h2>
-      <h3 className={EducationCSS.cardTime}>
-        {timeFrom} - {timeTo}
-      </h3>
-      <p className={EducationCSS.cardDesc}>{desc}</p>
-    </div>
-  );
-}
 
 export default () => {
   const cards = EducationData.map((item) => (
