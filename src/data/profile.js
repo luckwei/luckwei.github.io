b62631352df6cc9a9a91aa8f3af8bd3add7bb037
luckwei <luckwei123@gmail.com>
2022-11-11 12:22:15 +1100

    isolated profile data, allowing for info to be extended

---
+++ src/data/profile.js
@@ -0,0 +1,11 @@
+export default {
+    name: "Luckwei Cheong",
+    title: "Data Science Graduate | React Student",
+    website: "luckwei.github.io",
+    emailAddress: "luckweicheong@gmail.com",
+    linkedInLink: "https://www.linkedin.com/in/luck-wei-cheong/",
+    info: {
+        about: "I'm a Data Science Masters Graduate who loves coding and is interested to learn web-development to increase my employability. I've always found React to be daunting, not being very experienced in JS, but I decided to take up the challenge anyways! With that, I've been surprised how well I've caught up and Scrimba has been an amazing platform for me to learn :D",
+        interests: "Other than coding and programming, I enjoy dancing! My main styles of interest are popping and hip hop. I also perform and cover Kpop choreographies with my friends in the Melbourne community!"
+    },
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,44 +13,34 @@ import {
   website,
 } from "../styles/pages/Home.module.css";
 
-export default () => (
-  <>
-    <img className={picture} src={myPhoto} />
-    <main className={profile}>
-      <h1 className={name}>Luckwei Cheong</h1>
-      <h3 className={title}>Data Science Graduate | React Student</h3>
-      <h4 className={website}>luckwei.github.io</h4>
-      <div className={connect}>
-        <a className={mailBtn} href="mailto:luckweicheong@gmail.com">
-          <img src={mailIcon} />
-          Email
-        </a>
-        <a
-          className={liBtn}
-          href="https://www.linkedin.com/in/luck-wei-cheong/"
-          target="_blank"
-        >
-          <img src={liIcon} />
-          LinkedIn
-        </a>
-      </div>
-      <div className={info}>
-        <h3>About</h3>
-        <p>
-          I'm a Data Science Masters Graduate who loves coding and is interested
-          to learn web-development to increase my employability. I've always
-          found React to be daunting, not being very experienced in JS, but I
-          decided to take up the challenge anyways! With that, I've been
-          surprised how well I've caught up and Scrimba has been an amazing
-          platform for me to learn :D
-        </p>
-        <h3>Interests</h3>
-        <p>
-          Other than coding and programming, I enjoy dancing! My main styles of
-          interest are popping and hip hop. I also perform and cover Kpop
-          choreographies with my friends in the Melbourne community!
-        </p>
-      </div>
-    </main>
-  </>
-);
+import profileData from "../data/profile";
+
+export default () => {
+  const infoSection = Object.entries(profileData.info).map(([key, value]) => (
+    <>
+      <h3>{key}</h3>
+      <p>{value}</p>
+    </>
+  ));
+  return (
+    <>
+      <img className={picture} src={myPhoto} />
+      <main className={profile}>
+        <h1 className={name}>{profileData.name}</h1>
+        <h3 className={title}>{profileData.title}</h3>
+        <h4 className={website}>{profileData.website}</h4>
+        <div className={connect}>
+          <a className={mailBtn} href={`mailto:${profileData.emailAddress}`}>
+            <img src={mailIcon} />
+            Email
+          </a>
+          <a className={liBtn} href={profileData.linkedInLink} target="_blank">
+            <img src={liIcon} />
+            LinkedIn
+          </a>
+        </div>
+        <div className={info}>{infoSection}</div>
+      </main>
+    </>
+  );
+};
