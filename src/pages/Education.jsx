b0b51af7ded30836d725ba8d2714fc58ccf19983
luckwei <luckwei123@gmail.com>
2022-11-11 11:48:36 +1100

    refactor css import

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,10 +1,10 @@
 import EducationCard from "../components/EducationCard"
-import EducationCSS from "../styles/pages/Education.module.css"
+import {education} from "../styles/pages/Education.module.css"
 import EducationData from "../data/education";
 
 export default () => {
   const cards = EducationData.map((item) => (
     <EducationCard key={item.id} item={item} />
   ));
-  return <div className={EducationCSS.education}>{cards}</div>;
+  return <div className={education}>{cards}</div>;
 };
