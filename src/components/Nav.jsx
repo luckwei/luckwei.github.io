3be8bdf31fd628f238af558213a282755d4d0578
luckwei <luckwei123@gmail.com>
2022-11-13 06:54:56 +1100

    moved footer and nav to index

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import globeIcon from "../assets/images/globe_icon.svg";
 import homeIcon from "../assets/images/home_icon.svg";
-import "../styles/components/Nav.module.sass";
 
 export default () => (
   <nav>
