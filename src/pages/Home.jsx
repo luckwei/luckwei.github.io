d26e9032970522f6a2e2dc2b9a4a86689eeb1569
luckwei <luckwei123@gmail.com>
2022-11-13 16:10:54 +1100

    profile photo now in main element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,24 +13,22 @@ export default () => {
     </div>
   ));
   return (
-    <>
+    <main className="home">
       <img src={myPhoto} />
-      <main className="home">
-        <h1 className="name">{profileData.name}</h1>
-        <h2 className="title">{profileData.title}</h2>
-        <h2 className="website">{profileData.website}</h2>
-        <div className="connect">
-          <a className="mailBtn" href={`mailto:${profileData.emailAddress}`}>
-            <img src={mailIcon} />
-            Email
-          </a>
-          <a className="liBtn" href={profileData.linkedInLink} target="_blank">
-            <img src={liIcon} />
-            LinkedIn
-          </a>
-        </div>
-        <div className="info">{infoSection}</div>
-      </main>
-    </>
+      <h1 className="name">{profileData.name}</h1>
+      <h2 className="title">{profileData.title}</h2>
+      <h2 className="website">{profileData.website}</h2>
+      <div className="connect">
+        <a className="mailBtn" href={`mailto:${profileData.emailAddress}`}>
+          <img src={mailIcon} />
+          Email
+        </a>
+        <a className="liBtn" href={profileData.linkedInLink} target="_blank">
+          <img src={liIcon} />
+          LinkedIn
+        </a>
+      </div>
+      <div className="info">{infoSection}</div>
+    </main>
   );
 };
